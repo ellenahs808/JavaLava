74bd3fb6b31e2833e460ae24187f15588c63348b
ellenahs808 <sgev808@hawaii.edu>
2021-03-01 20:40:52 -0800

    Add onlyPrimes method

--- src/miniprojects/PrimeDirective.java
+++ src/miniprojects/PrimeDirective.java
@@ -2,7 +2,7 @@ package miniprojects;
 import java.util.ArrayList;
 
 public class PrimeDirective {
-    public boolean isPrime(int number) {
+    public boolean isPrime(int number) { //isPrime method that takes in a number and returns a boolean
         if (number == 2) {
             return true;
         } else if (number < 2) {
@@ -18,6 +18,19 @@ public class PrimeDirective {
     }
 
 
+    public ArrayList<Integer> onlyPrimes(int[] numbers) { //onlyPrimes method that takes in an array and returns an ArrayList<Integer>
+//        ArrayList<Integer> primes = new ArrayList<Integer>();  //instantiate a new ArrayList called primes
+        ArrayList<Integer> primes = new ArrayList<>(); // can also write it this way where you dont have to put explicit value of ArrayList when you instantiate
+
+        for (int number : numbers) { // "for each number in numbers" - this is iterating through an array
+            if (isPrime(number)) {
+                primes.add(number);
+            }
+        }
+        return primes;
+    }
+
+
     public static void main(String[] args) {
         PrimeDirective pd = new PrimeDirective();
         int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
@@ -25,5 +38,7 @@ public class PrimeDirective {
         System.out.println(pd.isPrime(28)); //false
         System.out.println(pd.isPrime(2)); //true
         System.out.println(pd.isPrime(0)); //false
+
+        System.out.println(pd.onlyPrimes(numbers)); // [29, 11, 101, 43, 89]
     }
 }

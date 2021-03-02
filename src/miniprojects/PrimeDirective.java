6a6a36a34cac6726501754bec53f99cb10c9c496
ellenahs808 <sgev808@hawaii.edu>
2021-03-01 20:30:00 -0800

    Add PrimeDirective class

---
+++ src/miniprojects/PrimeDirective.java
@@ -0,0 +1,9 @@
+package miniprojects;
+import java.util.ArrayList;
+
+public class PrimeDirective {
+//    public static void main(String[] args) {
+//        PrimeDirective pd = new PrimeDirective();
+//        int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+//    }
+}

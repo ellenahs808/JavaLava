e0cea688b603832daccbb92366d0c36624c6a03a
ellenahs808 <sgev808@hawaii.edu>
2021-03-07 18:55:19 -0800

    Add polymorphism folder

---
+++ src/miniprojects/polymorphism/Noodle.java
@@ -0,0 +1,39 @@
+package miniprojects.polymorphism;
+
+public class Noodle {
+
+    protected double lengthInCentimeters;
+    protected double widthInCentimeters;
+    protected String shape;
+    protected String ingredients;
+    protected String texture = "brittle";
+
+    Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
+
+        this.lengthInCentimeters = lenInCent;
+        this.widthInCentimeters = wthInCent;
+        this.shape = shp;
+        this.ingredients = ingr;
+
+    }
+
+    public String getCookPrep() {
+
+        return "Boil noodle for 7 minutes and add sauce.";
+
+    }
+
+    public static void main(String[] args) {
+
+        Noodle spag, pho;
+        spag = new Spaghetti();
+        // ramen = new Ramen();
+        Ramen ramen = new Ramen();
+        pho = new Pho();
+
+        System.out.println(spag.getCookPrep());
+        System.out.println(ramen.onlyRamen());
+
+    }
+
+}

f15bfd6d9e88f5ecab319cd155d6248eb64cfa74
ellenahs808 <sgev808@hawaii.edu>
2021-03-07 20:26:46 -0800

    Add SinoTibetan class

---
+++ src/miniprojects/languages/SinoTibetan.java
@@ -0,0 +1,12 @@
+package miniprojects.languages;
+
+class SinoTibetan extends Language {
+    SinoTibetan(String languageName, int numSpeakers) {
+        super(languageName, numSpeakers, "Asia", "subject-object-verb");
+        if (languageName.contains("Chinese")) {
+            this.wordOrder = "subject-verb-object";
+        }
+    }
+
+
+}

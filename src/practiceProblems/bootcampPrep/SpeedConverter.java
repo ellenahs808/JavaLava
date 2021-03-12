cab34e494a2a55a2d880f30f59790fbd8beec5f1
ellenahs808 <sgev808@hawaii.edu>
2021-03-11 22:43:44 -0800

    Add test cases for SpeedConverter class

--- src/practiceProblems/bootcampPrep/SpeedConverter.java
+++ src/practiceProblems/bootcampPrep/SpeedConverter.java
@@ -6,9 +6,23 @@ public class SpeedConverter {
         if (kilometersPerHour < 0) {
             return -1;
         } else {
-            return Math.round(kilometersPerHour * 0.6214);
+            return Math.round(kilometersPerHour / 1.609);
+        }
+
+    }
+
+
+    public void printConversion(double kilometersPerHour) {
+        long conversion = toMilesPerHour(kilometersPerHour);
+        // "XX km/h = YY mi/h"
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " +  conversion + " mi/h");
         }
     }
+
+
 
 
     public static void main(String[] args) {
@@ -19,11 +33,15 @@ public class SpeedConverter {
         System.out.println(toMilesPerHour(75.114));  //→ should return value 47);
 
 
+        SpeedConverter test1 = new SpeedConverter();
 
-//        toMilesPerHour(1.5);  //→ should return value 1
-//        toMilesPerHour(10.25);  //→ should return value 6
-//        toMilesPerHour(-5.6);  //→ should return value -1
-//        toMilesPerHour(25.42);  //→ should return value 16
-//        toMilesPerHour(75.114);  //→ should return value 47
+        test1.printConversion(1.5);   //→ should print the following text (into the console - System.out): 1.5 km/h = 1 mi/h
+        test1.printConversion(10.25);   //→ should print the following text (into the console - System.out): 10.25 km/h = 6 mi/h
+        test1.printConversion(-5.6);   //→ should print the following text (into the console - System.out): Invalid Value
+        test1.printConversion(25.42);   //→ should print the following text (into the console - System.out): 25.42 km/h = 16 mi/h
+        test1.printConversion(75.114);   //→ should print the following text (into the console - System.out): 75.114 km/h = 47 mi/h
+
+
+
     }
 }
